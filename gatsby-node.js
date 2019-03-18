ca2b39ae5fe608c0c9407ecfd358917c19cfe18d
iljapanic <ilja@iljapanic.com>
2019-03-18 23:19:37 +0100

    rewrite gatsby-node.js back to promises, but this with with graphql aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,211 +2,196 @@ const _ = require('lodash');
 const path = require('path');
 const Promise = require('bluebird');
 const { createFilePath } = require(`gatsby-source-filesystem`);
-const postTemplate = path.resolve(`./src/templates/post.js`);
 
-exports.createPages = async function({actions, graphql}) {
-  const { createPage } = actions
+// exports.createPages = async function({actions, graphql}) {
+//   const { createPage } = actions
 
-  await graphql(`    {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 1000
-      filter: {
-        frontmatter: { published: { eq: true } }
-        fileAbsolutePath: { regex: "/reflections/" }
-      }
-    ) {
-      edges {
-        node {
-          frontmatter {
-            slug
-            title
-          }
-        }
-      }
-    }
-  }
-  `
-).then(result => {
-  if (result.errors) {
-    console.log(result.errors);
-    reject(result.errors);
-  }
-
-  // create Reflections pages
-
-  const reflections = result.data.allMarkdownRemark.edges;
-
-  _.each(reflections, (reflection, index) => {
-    const previous = index === reflections.length - 1 ? null : reflections[index + 1].node;
-    const next = index === 0 ? null : reflections[index - 1].node;
-
-    createPage({
-      path: `reflections/${reflection.node.frontmatter.slug}`,
-      component: postTemplate,
-      context: {
-        slug: reflection.node.frontmatter.slug,
-        previous,
-        next
-      }
-    });
-  });
-  })
-}
-
-exports.createPages = async function({actions, graphql}) {
-  const { createPage } = actions
-
-  await graphql(`    {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 1000
-      filter: {
-        frontmatter: { published: { eq: true } }
-        fileAbsolutePath: { regex: "/fabacademy/" }
-      }
-    ) {
-      edges {
-        node {
-          frontmatter {
-            slug
-            title
-          }
-        }
-      }
-    }
-  }
-  `
-).then(result => {
-  if (result.errors) {
-    console.log(result.errors);
-    reject(result.errors);
-  }
-
-  // create Reflections pages
-
-  const fabacademy = result.data.allMarkdownRemark.edges;
-
-  _.each(fabacademy, (post, index) => {
-    const previous = index === fabacademy.length - 1 ? null : fabacademy[index + 1].node;
-    const next = index === 0 ? null : fabacademy[index - 1].node;
-
-    createPage({
-      path: `fabacademy/${post.node.frontmatter.slug}`,
-      component: postTemplate,
-      context: {
-        slug: post.node.frontmatter.slug,
-        previous,
-        next
-      }
-    });
-  });
-  })
-}
-
-
-
-
-// exports.createPages = async function({ actions, graphql }) {
-//   const { createPage } = actions;
-
-//   return new Promise((resolve, reject) => {
-//     const postTemplate = path.resolve(`./src/templates/post.js`);
-
-//     resolve(
-//       graphql(`
-//         {
-//           allMarkdownRemark(
-//             sort: { order: DESC, fields: [frontmatter___date] }
-//             limit: 1000
-//             filter: {
-//               frontmatter: { published: { eq: true } }
-//               fileAbsolutePath: { regex: "/reflections/" }
-//             }
-//           ) {
-//             edges {
-//               node {
-//                 frontmatter {
-//                   slug
-//                   title
-//                 }
-//               }
-//             }
+//   await graphql(`    {
+//     allMarkdownRemark(
+//       sort: { order: DESC, fields: [frontmatter___date] }
+//       limit: 1000
+//       filter: {
+//         frontmatter: { published: { eq: true } }
+//         fileAbsolutePath: { regex: "/reflections/" }
+//       }
+//     ) {
+//       edges {
+//         node {
+//           frontmatter {
+//             slug
+//             title
 //           }
 //         }
-//       `).then(result => {
-//         if (result.errors) {
-//           console.log(result.errors);
-//           reject(result.errors);
-//         }
+//       }
+//     }
+//   }
+//   `
+// ).then(result => {
+//   if (result.errors) {
+//     console.log(result.errors);
+//     reject(result.errors);
+//   }
 
-//         // create Reflections pages
+//   // create Reflections pages
 
-//         const reflections = result.data.allMarkdownRemark.edges;
+//   const reflections = result.data.allMarkdownRemark.edges;
 
-//         _.each(reflections, (reflection, index) => {
-//           const previous = index === reflections.length - 1 ? null : reflections[index + 1].node;
-//           const next = index === 0 ? null : reflections[index - 1].node;
+//   _.each(reflections, (reflection, index) => {
+//     const previous = index === reflections.length - 1 ? null : reflections[index + 1].node;
+//     const next = index === 0 ? null : reflections[index - 1].node;
 
-//           createPage({
-//             path: `reflections/${reflection.node.frontmatter.slug}`,
-//             component: postTemplate,
-//             context: {
-//               slug: reflection.node.frontmatter.slug,
-//               previous,
-//               next
-//             }
-//           });
-//         });
-//       })
-//     );
-
-//     resolve(
-//       graphql(`
-//         {
-//           allMarkdownRemark(
-//             sort: { order: DESC, fields: [frontmatter___date] }
-//             limit: 1000
-//             filter: {
-//               frontmatter: { published: { eq: true } }
-//               fileAbsolutePath: { regex: "/fabacademy/" }
-//             }
-//           ) {
-//             edges {
-//               node {
-//                 frontmatter {
-//                   slug
-//                   title
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `).then(result => {
-//         if (result.errors) {
-//           console.log(result.errors);
-//           reject(result.errors);
-//         }
-
-//         // create Reflections pages
-
-//         const fabacademy = result.data.allMarkdownRemark.edges;
-
-//         _.each(fabacademy, (post, index) => {
-//           const previous = index === fabacademy.length - 1 ? null : fabacademy[index + 1].node;
-//           const next = index === 0 ? null : fabacademy[index - 1].node;
-
-//           createPage({
-//             path: `fabacademy/${post.node.frontmatter.slug}`,
-//             component: postTemplate,
-//             context: {
-//               slug: post.node.frontmatter.slug,
-//               previous,
-//               next
-//             }
-//           });
-//         });
-//       })
-//     );
+//     createPage({
+//       path: `reflections/${reflection.node.frontmatter.slug}`,
+//       component: postTemplate,
+//       context: {
+//         slug: reflection.node.frontmatter.slug,
+//         previous,
+//         next
+//       }
+//     });
 //   });
-// };
+//   })
+// }
+
+// exports.createPages = async function({actions, graphql}) {
+//   const { createPage } = actions
+
+//   await graphql(`    {
+//     allMarkdownRemark(
+//       sort: { order: DESC, fields: [frontmatter___date] }
+//       limit: 1000
+//       filter: {
+//         frontmatter: { published: { eq: true } }
+//         fileAbsolutePath: { regex: "/fabacademy/" }
+//       }
+//     ) {
+//       edges {
+//         node {
+//           frontmatter {
+//             slug
+//             title
+//           }
+//         }
+//       }
+//     }
+//   }
+//   `
+// ).then(result => {
+//   if (result.errors) {
+//     console.log(result.errors);
+//     reject(result.errors);
+//   }
+
+//   // create Reflections pages
+
+//   const fabacademy = result.data.allMarkdownRemark.edges;
+
+//   _.each(fabacademy, (post, index) => {
+//     const previous = index === fabacademy.length - 1 ? null : fabacademy[index + 1].node;
+//     const next = index === 0 ? null : fabacademy[index - 1].node;
+
+//     createPage({
+//       path: `fabacademy/${post.node.frontmatter.slug}`,
+//       component: postTemplate,
+//       context: {
+//         slug: post.node.frontmatter.slug,
+//         previous,
+//         next
+//       }
+//     });
+//   });
+//   })
+// }
+
+
+
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  return new Promise((resolve, reject) => {
+
+    const postTemplate = path.resolve(`./src/templates/post.js`);
+
+    resolve(
+      graphql(`
+        {
+          reflections: allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            limit: 1000
+            filter: {
+              frontmatter: { published: { eq: true } }
+              fileAbsolutePath: { regex: "/reflections/" }
+            }
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  slug
+                  title
+                }
+              }
+            }
+          }
+          fabacademy: allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            limit: 1000
+            filter: {
+              frontmatter: { published: { eq: true } }
+              fileAbsolutePath: { regex: "/fabacademy/" }
+            }
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  slug
+                  title
+                }
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          console.log(result.errors);
+          reject(result.errors);
+        }
+
+        // create Reflections pages
+
+        const reflections = result.data.reflections.edges;
+        const fabacademy = result.data.fabacademy.edges;
+
+        _.each(reflections, (reflection, index) => {
+          const previous = index === reflections.length - 1 ? null : reflections[index + 1].node;
+          const next = index === 0 ? null : reflections[index - 1].node;
+
+          createPage({
+            path: `reflections/${reflection.node.frontmatter.slug}`,
+            component: postTemplate,
+            context: {
+              slug: reflection.node.frontmatter.slug,
+              previous,
+              next
+            }
+          });
+        });
+
+        _.each(fabacademy, (post, index) => {
+          const previous = index === fabacademy.length - 1 ? null : fabacademy[index + 1].node;
+          const next = index === 0 ? null : fabacademy[index - 1].node;
+
+          createPage({
+            path: `fabacademy/${post.node.frontmatter.slug}`,
+            component: postTemplate,
+            context: {
+              slug: post.node.frontmatter.slug,
+              previous,
+              next
+            }
+          });
+        });
+      })
+    );
+  });
+};
