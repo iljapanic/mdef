41e276ccdf40c700917c1c1b5c7fcedc3559b3c9
iljapanic <ilja@iljapanic.com>
2020-06-01 01:04:19 +0200

    remove firebase dependencies

--- src/pages/dialogues/plant-monitor.js
+++ src/pages/dialogues/plant-monitor.js
@@ -3,13 +3,11 @@ import { Helmet } from 'react-helmet';
 
 // components
 import Dialogues from '../../components/Dialogues';
-import Charts from '../../components/Charts';
 
 // index.js
 export default ({ children }) => {
   return (
     <Dialogues>
-      <Charts />
       <Helmet>
         <title>Plant Monitor</title>
       </Helmet>
